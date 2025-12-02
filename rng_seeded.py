518ffe130f75c36e4fa0b31cefd14f5fb502a982
KayGJay <92559839+KayGJay@users.noreply.github.com>
2025-12-01 21:26:01 -0600

    Update rng_seeded.py

--- rng_seeded.py
+++ rng_seeded.py
@@ -1,7 +1,8 @@
 import random
 import numpy as np
 
-print("Without seed:")
+print("With seed:")
+random.seed(42)
 print(random.random())
-rng = np.random.default_rng()
+rng = np.random.default_rng(42)
 print(rng.random())
