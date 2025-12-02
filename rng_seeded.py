1f888a24b01d3593f3021330bda58671bfbbad4d
KayGJay <92559839+KayGJay@users.noreply.github.com>
2025-12-01 21:19:56 -0600

    Remove fixed seed and update random number generation

--- rng_seeded.py
+++ rng_seeded.py
@@ -3,8 +3,5 @@ import numpy as np
 
 print("Without seed:")
 print(random.random())
-print(random.randint(1, 10))
-
-rng = np.random.default_rng(42)
-
+rng = np.random.default_rng()
 print(rng.random())
