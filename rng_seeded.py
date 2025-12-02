8810031b241454a29730a5c8ec3579310626db8c
KayGJay <92559839+KayGJay@users.noreply.github.com>
2025-12-01 20:52:51 -0600

    Remove print statement for seedless random output

--- rng_seeded.py
+++ rng_seeded.py
@@ -2,8 +2,6 @@ import random
 import numpy as np
 
 #Edit to trigger
-print("Edit")
-# Without setting a seed (results will vary each run)
 print("Without seed:")
 print(random.random())
 print(random.randint(1, 10))
